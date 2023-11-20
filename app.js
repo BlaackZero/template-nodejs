8ad0fb4b26cff5f05472fcd63de8e25f8be4ef11
felipeosorioslier <blaackzeero@gmail.com>
2023-11-20 00:39:57 -0300

    Update

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
-const port = process.env.PORT ?? 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static('public'))
+app.use(cors({ origin: '*', credentials: true }));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  socket.on('playVideo', (data) => {
+    io.emit('playVideo', data);
+    console.log(data);
+  });
+
+  socket.on('toggleVideo', () => {
+    io.emit('toggleVideo');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Servidor WebSocket en el puerto ${PORT}`);
+});
